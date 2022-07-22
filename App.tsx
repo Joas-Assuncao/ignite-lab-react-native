c89743e2c370e565cb1244cd8cee616bb51ddf1e
Joas-Assuncao <joassalvior@gmail.com>
2022-07-22 15:13:38 -0300

    project finished with ROCKETSEAT

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+
+import { Routes } from './src/routes';
+
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/theme';
+
+export default function App() {
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+
+            { fontsLoaded ? <Routes /> : <Loading />}
+        </NativeBaseProvider>
+    );
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import { Button as NBButton, Heading, IButtonProps } from 'native-base';
+import React from 'react';
+
+type ButtonProps = IButtonProps & {
+    title: string;
+}
+
+export function Button({ title, ...rest }: ButtonProps) {
+    return (
+        <NBButton
+            bg="green.700"
+            h={14}
+            fontSize="sm"
+            rounded="sm"
+            _pressed={{ bg: "green.500" }}
+            { ...rest }
+        >
+            <Heading color="white" fontSize="xl">
+                {title}
+            </Heading>
+        </NBButton>
+    )
+}
